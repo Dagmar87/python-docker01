c7f00809ef89f4b672156e7b0e7d2e9e243b5d76
Dagmar87 <jdfssobrinho@hotmail.com>
2022-02-10 16:19:32 -0300

    Primeira commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from redis import Redis
+import os
+
+if os.environ.get('HOST_REDIS'):
+    host_redis = os.environ.get('HOST_REDIS')
+else:
+    host_redis = 'redis'
+    
+if os.environ.get('PORT_REDIS'):
+    port_redis = os.environ.get('PORT_REDIS')
+else:
+    port_redis = 6379
+    
+app = Flask(__name__)
+redis = Redis(host = host_redis, port = port_redis)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'Hello World! %s times.' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = true)
